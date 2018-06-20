37ffe1a61e156165dd0ab084b9ac51c3ff035029
Kamen <kamen.boev@gmail.com>
2018-06-20 15:45:11 +0300

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -3,7 +3,6 @@ const input = [2, 3, 2, -1, 0, 5, 4, -2, 6, 5, 23, 3, 9];
 // the function is checking every sum
 const additions = (array, number) => {
 	let len = array.length;
-	let right = [];
 	let answer = [];
 	for (var i = 0; i < len; i++) {
 		for (var j = i + 1; j < len; j++) {
