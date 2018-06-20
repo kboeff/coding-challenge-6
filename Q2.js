7399b0f6ef8dda5bf73c2ed7076d0eb19ca95121
Kamen <kamen.boev@gmail.com>
2018-06-20 10:49:27 +0300

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,13 +1,13 @@
 const input = [2, 3, 2, -1, 0, 5, 4, -2, 6, 5, 23, 3, 9];
 
-// checking every sum
+// the function is checking every sum
 const additions = (array, number) => {
 	let len = array.length;
 	let right = [];
 	let answer = [];
 	for (var i = 0; i < len; i++) {
 		for (var j = i + 1; j < len; j++) {
-
+			// checks the sums and also if the couple is already
 			if (array[i] + array[j] === number 
 				&& !(answer.includes(array[i], array[j])) 
 				&& !(answer.includes(array[j], array[i]))) {
